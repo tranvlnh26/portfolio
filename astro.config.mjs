4a04341906a5ee703301b5f6d2178ae2664b71a9
tranvlnh <92441399+tranvlnh26@users.noreply.github.com>
2025-12-08 21:43:25 +0700

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,35 @@
 import mdx from "@astrojs/mdx";
 import netlify from "@astrojs/netlify";
 import react from "@astrojs/react";
-import { defineConfig, envField } from "astro/config";
-
 import favicons from "astro-favicons";
+import { defineConfig, envField } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), mdx(), favicons()],
+  integrations: [
+    react(),
+    mdx(),
+    favicons({
+      input: ["public/favicon.svg", "public/favicon.png"],
+      name: "tranvlnh",
+      icons: {
+        favicons: true,
+        android: true,
+        appleIcon: true,
+        appleStartup: true,
+        windows: true,
+        yandex: true,
+      },
+      pixel_art: true,
+      manifestMaskable: false,
+      output: {
+        images: true,
+        files: true,
+        html: true,
+        assetsPrefix: "/public/",
+      },
+    }),
+  ],
   adapter: netlify(),
   env: {
     schema: {

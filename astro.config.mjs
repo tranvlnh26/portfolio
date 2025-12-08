b8387bb0aeafaea121482fd2b1d1cab0c599823d
tranvlnh <92441399+tranvlnh26@users.noreply.github.com>
2025-12-08 20:59:30 +0700

    Initial commit with Astro portfolio template
    Add base Astro portfolio project including configuration files, components, content, and assets. Includes .gitignore, Prettier config, Astro config, initial pages, components, and example blog content. Sets up project structure for further development.

---
+++ astro.config.mjs
@@ -0,0 +1,18 @@
+// @ts-check
+import mdx from "@astrojs/mdx";
+import netlify from "@astrojs/netlify";
+import react from "@astrojs/react";
+import { defineConfig, envField } from "astro/config";
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [react(), mdx()],
+  adapter: netlify(),
+  env: {
+    schema: {
+      CLIENT_ID: envField.string({ context: "server", access: "secret" }),
+      CLIENT_SECRET: envField.string({ context: "server", access: "secret" }),
+      REFRESH_TOKEN: envField.string({ context: "server", access: "secret" }),
+    },
+  },
+});
---
+++ src/content/config.ts
@@ -0,0 +1,16 @@
+import { defineCollection, z } from "astro:content";
+
+const blog = defineCollection({
+  type: "content",
+  schema: z.object({
+    title: z.string(),
+    description: z.string(),
+    publishDate: z.date(),
+    updatedDate: z.date().optional(),
+    tags: z.array(z.string()).default([]),
+    draft: z.boolean().default(false),
+  }),
+});
+
+export const collections = { blog };
+

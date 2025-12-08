a792ea9d85a1c093a71dfa842948ecafc9741802
tranvlnh <92441399+tranvlnh26@users.noreply.github.com>
2025-12-08 21:09:48 +0700

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import { defineConfig, envField } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://tranvlnh26.github.io/",
   base: "/portfolio",
   integrations: [react(), mdx()],
   adapter: netlify(),
